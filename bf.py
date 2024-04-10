cc01ab7ced71c8e12dcdc1bd5636e166e11b6869
SrujanBhirud <138774014+SrujanBhirud@users.noreply.github.com>
2024-04-10 21:12:24 +0530

    Update bf.py

--- bf.py
+++ bf.py
@@ -1,18 +1,23 @@
 import sys
+import os
 
-def run_brainfuck(filename):
-    try:
-        with open(filename, 'r') as file:
-            bf_code = file.read()
-    except FileNotFoundError:
-        print(f"Error: File '{filename}' not found.")
-        return
-        
-    user_input = []
+def run_brainfuck(bf_code,user_input):
+    user_input = list(user_input)
 
     loop_stack = []
+    loop_table = {}
     memory = [0]
     mem_pointer = 0
+    output = ""
+    
+    for bf_pointer, bf_op in enumerate(bf_code):
+        if bf_op == "[":
+            loop_stack.append(bf_pointer)
+        elif bf_op == "]":
+            start = loop_stack.pop()
+            loop_table[start] = bf_pointer
+            loop_table[bf_pointer] = start
+
     bf_pointer = 0
 
     while (bf_pointer < len(bf_code)):
@@ -20,17 +25,11 @@ def run_brainfuck(filename):
         val = memory[mem_pointer]
 
         if bf_op == "+":
-            if val == 255:
-                val = 0
-            else:
-                val += 1
+            val = (val + 1) %256
             memory[mem_pointer] = val
 
         elif bf_op == "-":
-            if val == 0:
-                val = 255
-            else:
-                val -= 1
+            val = (val - 1) %256
             memory[mem_pointer] = val
 
         elif bf_op == ">":
@@ -44,34 +43,37 @@ def run_brainfuck(filename):
 
         elif bf_op == ",":
             if(user_input == []):
-                user_input = list(input())
+                break
             val = ord(user_input.pop(0))
+            memory[mem_pointer] = val
         
         elif bf_op == ".":
-            print(chr(val), end = "")
+            output += chr(val)
 
         elif bf_op == "[":
-            loop_stack.append(bf_pointer)
             if(val == 0):
-                while(len(loop_stack) != 0):
-                    bf_pointer += 1
-                    if bf_code[bf_pointer] == "]":
-                        loop_stack.pop()
-                    elif bf_code[bf_pointer] == "[":
-                        loop_stack.append(bf_pointer)   
+                bf_pointer = loop_table[bf_pointer]  
 
         elif bf_op == "]":
             if(val != 0):
-                bf_pointer = loop_stack[-1]
-            else:
-                loop_stack.pop()
+                bf_pointer =loop_table[bf_pointer]
 
         
         bf_pointer += 1
+    return output
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python bf.py <filename.bf>")
-    else:
+    try:
         filename = sys.argv[1]
-        run_brainfuck(filename)
+        if not os.path.isfile(filename):
+            raise FileNotFoundError(f"Error: File '{filename}' not found.")
+    except IndexError:
+        print("Error: Please provide only the filename of the BrainFuck interpreter and your bf code")
+        print("Usage: python <bf_interpreter.py> <filename.txt>")
+    except:
+        print("Usage: python <bf_interpreter.py> <filename.txt>")
+    else:
+        with open(filename, 'r') as file:
+            bf_code = file.read()
+            print(run_brainfuck(bf_code,input("Enter user input. Press Enter to skip: ")))
+
